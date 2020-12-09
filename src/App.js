b327e35c2e648dd82e5d15594393556a305e6e85
Raeel Haider <engraeelhaider@gmail.com>
2020-12-09 08:27:40 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import './App.css';
 function App() {
   let [count, setCount] = useState(0);
   let [isMorning, setMorning] = useState(true);
+
    return (
     <div className = {`box ${isMorning ? 'Daylight' : 'Night' }`}>
       <h1> Good {isMorning ? 'Morning' : 'Night'} </h1>
       <Message counter = {count} />
       <br/>
       <button onClick = { ()  => setCount(++count)
+      <button onClick = { () => setCount(--count)
+      } >
+        Reverse Counter </button>
        } > 
       Update Counter</button>
 
